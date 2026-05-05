e5a4ad251cdbaf93ce402a8bd77982687ea28676
rakibdev <rakibdev01@gmail.com>
2026-05-05 18:48:15 +0600

    remove zig

--- zig-app/src/gtk.zig
+++
@@ -1,9 +0,0 @@
-pub usingnamespace @cImport({
-    @cInclude("gtk/gtk.h");
-    @cInclude("gtk-layer-shell.h");
-    @cInclude("cairo/cairo.h");
-    @cInclude("librsvg/rsvg.h");
-    @cInclude("webp/decode.h");
-    @cInclude("jpeglib.h");
-    @cInclude("glib.h");
-});
